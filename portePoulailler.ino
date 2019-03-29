f33420beb58559ad08d2a17921e6b40be50f4f46
Cedric <gronnier.cedric@live.fr>
2019-03-29 14:58:19 +0100

    Avancee du jour.

--- portePoulailler.ino
+++ portePoulailler.ino
@@ -20,24 +20,31 @@ int extra = 200;                 //Nombre de pas a faire en plus apres ouverture
 int seuil = 750;              //Seuil de declenchement ouverture/fermeture porte.
 int lumi;               //Variable qui stock la valeur de luminosité ambiante
 long delaiTimer = 0;    //Pour valider que le sueil est bien franchi. evite les ouv/ferm intempestives
-int validation = 0;     //Indique que l'on est en attende de validation que le seuil franchi
+int validation = 15000;     //Indique que l'on est en attende de validation que le seuil franchi
 boolean hyst = 50;      //Hysteresis
 long delai = 2000;      //Delai de validation 
 
 //********** Declaration des variables pour la gestion de la LED *********************************************
-int delaiOn = 0;
-int delaiOff = 0;
-int nbCycles = 0;
-long timerOn;
-long timerOff;
+int delaiOn = 0;         //Duree LED allumee
+int delaiOff = 0;        //Duree LED eteinte
+int nbCycles = 0;        //Nombre de cycles a realiser
+long timerOn;            //Timer LED alluméé
+long timerOff;           //Timer LED eteinte
 boolean etatLed = 0;     //0 = LED eteinte, 1 = LED allumee
 boolean flashMode = 0;   //Indique que la LED doit clignoter
-int flashType = 1;
-int pwm = 0;             //utilisé pour alumage et extinction progressive
+int flashType = 1;       //Mode de clignotement
+int pwm = 0;             //Utilisé pour alumage et extinction progressive
+long timerProgressif;    //Timer 
+long delaiProgressif;    //Delai pour passer de eteint a allume
+int delaiOffCode;        //Delai entre deux repetition du code
+long timerOffCode;       //Timer delai entre deux repetitions du code
+int code;                //Nombre de flash du code a emettre
+int compteurCode;        //Decompte le nombre de flash emis
+boolean pause = 0;       //Sert à distinguer la pause entre deux emission du code
 
-void setup() 
+void setup() //**********************************************************************************************
 {
-  Serial.begin(9600);          //Utilisé pour debug  
+  Serial.begin(9600);                       //Utilisé pour debug  
   pinMode(Step, OUTPUT);
   pinMode(Dir, OUTPUT);
   pinMode(Enable, OUTPUT);
@@ -46,84 +53,112 @@ void setup()
   etatFdcHaut = digitalRead(FdcHaut);
   etatFdcBas = digitalRead(FdcBas);
   digitalWrite(Enable, HIGH);
-  flashConfig(500, 500, 5);  //debug
+  ledConfig(2, 1800, 1000, 20000);     //debug
 }
 
 //**********************************************************************************************************
 //*** Utilisation de la fonction ledConfig()                                                             
-//*** Mode: - 1 = LED clignotante, on: duree allumee, off: duree eteinte, nb: nombre de clignotements
-//***       - 2 = LED allumage progressif, on: duree pour passer de  eteint a allumee, off: duree durant
-//***             laquelle le LED est eteinte entre deux cycles.
-//***       - 3 = LED a extinction progressive: idem allumage progressif mais a l'envers pour le on.
+//*** Mode: - 1 = LED clignotante, 'a': duree allumee, 'b': duree eteinte, 'c': nombre de clignotements
+//***       - 2 = LED allumage progressif, 'a': duree pour passer de  eteint a allumee, 'b': duree durant
+//***             laquelle le LED est eteinte entre deux cycles, 'c': duree du mode en ms
+//***       - 3 = LED a extinction progressive: idem allumage progressif mais a l'envers pour le 'a'.
+//***       - 4 = Code: 'a': nb. de flash du code, 'b': duree du flash, 'c':duree d'emission du code en ms.
 //***********************************************************************************************************
-void ledConfig(int mode, int on, int off, int nb)
+void ledConfig(int mode, int a, int b, long c)
 {
-  delaiOn = on;
-  delaiOff = off;
-  nbCycles = nb;
-  flashType = mode;
-  timerOn = millis();
-  flashMode = 1;
-  if(flashType == 1)
+  delaiOn = a;                      //Defini delaiOn suivant parametre de la fonction
+  delaiOff = b;                     //Defini delaiOff suivant parametre de la fonction
+  nbCycles = c;                     //Defini nb suivant parametre de la fonction
+  flashType = mode;                 //Defini flashType suivant parametre de la fonction
+  timerOn = millis();               //Demarre le timer On
+  flashMode = 1;                    //Indique que la Led doit clignoter
+  if(flashType == 1)                //Si mode Clignotement
   {
-    analogWrite(LED, 255);
-    etatLed = 1;
+    analogWrite(LED, 255);          //Allume la LED
+    etatLed = 1;                    //Defini son etat comme allumee
   }
-  if((flashType == 2)
+  if((flashType == 2))              //Si mode allumage pregressif
   {
-    delaiOn = (on / 255);
-    pwm = 0;
+    delaiOn = (delaiOn / 255);      //Divise delaiOn par 255 pour calculer la duree du pas d'incrementation du rapport cyclique pour l'allumage progressif
+    pwm = 0;                        //Initialise PWM a 0
+    etatLed = 1;                    //Defini etat LED comme allumee
+    delaiProgressif = c;            //Defini delaiProgressif  suivant parametre de la fonction
+    timerProgressif = millis();     //Demarre le timer du mode progressif 
   }
-  if((flashType == 3)
+  if((flashType == 3))              //Si mode extiction Progressive
   {
-    delaiOn = (on / 255);
-    pwm = 255;
+    delaiOn = (delaiOn / 255);      //Divise le delai Off pour calculer la duree du pas d'incrementation du rapport cyclique pour l'allumage progressif       
+    pwm = 255;                      //Initialise le PWM a 255
+    etatLed = 1;                    //Defini la LED comme allumee
+    delaiProgressif = c;            //Defini delaiProgressif suivant parametre de la fonction   
+    timerProgressif = millis();     //Demarre le timer du mode progressif
+  }
+  if(flashType == 4)                //Si mode code.
+  {
+    code = a;                       //Defini le nombre de lash du code
+    compteurCode = a;               //Initialise le compteur du code
+    delaiOn = b;                    //Defini la duree aluumee de la LED
+    delaiOff = b;                   //Defini la duree eteinte entre deux flash de la LED 
+    delaiOffCode = (b * 2);         //Defini la duree entre deux repetitions du code
+    analogWrite(LED, 255);          //Alulume la LED
+    etatLed = 1;                    //Defini l'etat LED comme allumee
+    flashMode = 1;                  //Indique que la LED doit clignoter
   }
 }
 
 void flash()  //*********************************************************************************************
 {
-  if(flashType == 1)
+  if(flashType == 1)                                            //Mode clignotement de la LED
   {
-    if(((millis() - timerOn) >= delaiOn) && (etatLed == 1))
+    if(((millis() - timerOn) >= delaiOn) && (etatLed == 1))     //Si delai LED allumee depassé
     {
-      timerOff = millis();
-      analogWrite(LED, 0);
-      etatLed = 0;
-      nbCycles--;
-      Serial.println("on");
+      analogWrite(LED, 0);                                      //Eteint la LED
+      timerOff = millis();                                      //Demarre le Timer Led eteinte
+      etatLed = 0;                                              //Defini etat led comme eteinte
+      nbCycles--;                                               //Decremente le compteur de clignotements                                  
     }
-    if(((millis() - timerOff) >= delaiOff) && (etatLed == 0))
+    if(((millis() - timerOff) >= delaiOff) && (etatLed == 0))   //Si delai LED eteinte depassé
     {
-      timerOn = millis();
-      analogWrite(LED, 255);
-      etatLed = 1;
-      Serial.println("off");
+      analogWrite(LED, 255);                                    //Allume la LED
+      timerOn = millis();                                       //Demarre le timer led allumee                     
+      etatLed = 1;                                              //Defini etat led comme allumee
     }
-    if(nbCycles <= 0)flashMode = 0;
+    if(nbCycles <= 0)flashMode = 0;                             //Si nombre de clignotement defini atteint, on arrete de faire clignoter la led
   }
-  if(flashType == 2)
+  if(flashType == 2)                                            //Mode allumage preogressif
   {
-    if(((millis() - delaiTimer) >= delaiOn) && (etatLed == 1))
+    if(((millis() - timerOn) >= delaiOn) && (etatLed == 1))     //Quand la duree d'un pas d'incrementation est depassé
     {
-      analogWrite(LED, pwm);
-      pwm++;
+      analogWrite(LED, pwm);                                    //Allume la LED au rapport cyclique defini au tour precedent
+      pwm++;                                                    //Incremente le rapport cyclique
+      timerOn = millis();                                       //Reinitialise le timer du pas d'incrementation
     }
-    if((pwm == 255) && (etatLed == 1))
+    if((pwm == 255) && (etatLed == 1))                          //Si la LED est eclairee a son maximum
     {
-      analogWrite(LED, 0);
-      timerOff = millis();
-      pwm = 0;
-      etatLed = 0;
+      analogWrite(LED, 0);                                      //Eteint la LED
+      timerOff = millis();                                      //Demarre le timer LED eteinte
+      pwm = 0;                                                  //Intialise le rapport cyclique a 0
+      etatLed = 0;                                              //Defini etat LED comme eteinte
     }
-    if((etatLed == 0) && ((millis() - timerOff) >= delaiOff)) etatLed = 1;
+    if((etatLed == 0) && ((millis() - timerOff) >= delaiOff))   //duree LED eteinte atteinte
+    {
+      etatLed = 1;                                              //defini etat LED comme allumee
+      timerOn = millis();                                       //Demarre le timer duree incrementation rapport cyclique
+    }
+    if((millis() - timerProgressif) >= delaiProgressif)         //La duree de clignotement de la led est atteinte
+    {
+      flashMode = 0;                                            //La LED ne doit plus clignoter
+      analogWrite(LED, 0);                                      //Eteint la LED
+      etatLed = 0;                                              //Etat LED defini comme eteinte
+    }
   }
-  if(flashType == 3)
+  if(flashType == 3)                                            //Mode extinction progressive
   {
-    if(((millis() - delaiTimer) >= delaiOn) && (etatLed == 1))
-    {
+    if(((millis() - timerOn) >= delaiOn) && (etatLed == 1))     //fonctionnement similaire a allumage progressif
+    {                                                           //Voir commentaire ci dessus
       analogWrite(LED, pwm);
       pwm--;
+      timerOn = millis();
     }
     if((pwm == 0) && (etatLed == 1))
     {
@@ -132,7 +167,46 @@ void flash()  //****************************************************************
       pwm = 255;
       etatLed = 0;
     }
-    if((etatLed == 0) && ((millis() - timerOff) >= delaiOff)) etatLed = 1;
+    if((etatLed == 0) && ((millis() - timerOff) >= delaiOff)) 
+    {
+      etatLed = 1;
+      timerOn = millis();
+    }
+    if((millis() - timerProgressif) >= delaiProgressif) 
+    {
+      flashMode = 0;
+      analogWrite(LED, 0);
+      etatLed = 0;
+    }
+  }
+  if(flashType == 4)                                                                                       //Mode code. 
+  {
+    if(((millis() - timerOn) >= delaiOn) && (etatLed == 1) && (compteurCode > 0))                          //Duree LED allumee atteinte
+    {
+      analogWrite(LED, 0);                                                                                 //eteint la LED
+      timerOff = millis();                                                                                 //Demarre le timer LED eteinte
+      etatLed = 0;                                                                                         //Defini etat LED comme eteinte
+      compteurCode--;                                                                                      //Decremente le compteur du nombre de flas du code
+    }
+    if(((millis() - timerOff) >= delaiOff) && (etatLed == 0) && (compteurCode > 0))                        //Duree LED eteinte atteint
+    {
+      analogWrite(LED, 255);                                                                               //Allume la LED
+      timerOn = millis();                                                                                  //Demarre le timer LED allumee 
+      etatLed = 1;                                                                                         //Definil'etat LED comme allumee
+    }
+    if(((millis() - timerOff) >= delaiOff) && (etatLed == 0) && (compteurCode == 0) &&(pause == 0))        //Si le nombre de flas du code a ete emis
+    {
+      timerOffCode = millis();                                                                             //Demarre le timer de la pause entre deux emission du code
+      pause = 1;                                                                                           //Defini que l'on se touve entre deux emission de code
+    }
+    if(((millis() - timerOffCode) >= delaiOffCode) && (pause == 1))                                        //Delai de pause entre deux emission du code ecoulee
+    {
+      analogWrite(LED, 255);                                                                               //Allume la LED
+      timerOn = millis();                                                                                  //Demarre le timer LED allumee
+      etatLed = 1;                                                                                         //defini etat LED comme allumee
+      compteurCode = code;                                                                                 //Initialise le compteur de flash du code a emettre         
+      pause = 0;                                                                                           //Defini la pause entre deux emission du code comme terminee
+    }
   }
 }
 
@@ -266,7 +340,7 @@ void luminosite() //************************************************************
 //  Serial.println(lumi);
 }
 
-void loop() 
+void loop() //***************************************************************************************************
 {
 //  luminosite();
   if(flashMode == 1) flash();
